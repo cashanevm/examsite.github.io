062f6e6d9e7fa2076bfc74c9c2eacf6e0f265d62
Nevm PC <Nevm PC@DESKTOP-EUP8F13>
2020-12-16 14:55:06 +0200

    FIRSR COMITВ

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,72 +1,50 @@
-async function getResponse (){
+async function getResponse (blockNum){
    
-    let num = localStorage.getItem("id")
+    let num = blockNum
   let response = await fetch(`https://examserverapp.herokuapp.com/users/${num}`)
   let content = await response.json()
   console.log(content[0].name)
+  let strtitle = ".title" + num
+  let strdisc =".disc" + num
+  let strimg =".img" + num
+  let title1= document.querySelector(strtitle)
+  let disc= document.querySelector(strdisc)
+  let img= document.querySelector(strimg)
+ 
   
   
-  let name= document.querySelector('.name')
-  let size= document.querySelector('.size')
-  let color= document.querySelector('.color')
-  let disc= document.querySelector('.disc')
-  let img= document.querySelector('.img')
-  let cost= document.querySelector('.cost')
   
+  title1.innerHTML=`${content[0].title1} `
+  disc.innerHTML=` ${content[0].disc} `
+  img.innerHTML=`  <img src="${content[0].img}" alt="" class="w-100">`
   
-  
-  name.innerHTML=`<h1>${content[0].name} </h1>`
-  size.innerHTML=`<h1>Розмір: ${content[0].size} </h1>`
-  color.innerHTML=`<h1>Колір: ${content[0].color} </h1>`
-  disc.innerHTML=`<h1>Відгуки: ${content[0].disc} </h1>`
-  img.innerHTML=`  <img src="${content[0].img}" alt="img">`
-  cost.innerHTML=`<h1> Ціна: ${content[0].cost} грн</h1>`
   
   }
 
 
-  function getColor(){
-    return "#"+(Math.floor(Math.random()*16777215)).toString(16);
-   }
-function getBody(){
-console.log("click");
-this.style.backgroundColor = getColor(); 
-this.style.transition = "4s" ;
-}
-//document.body.addEventListener("click",getBody);
-//document.body.addEventListener("mouseover",getDiv);
-function getDiv(){
-let div  = document.createElement("div"); 
-div.style.borderRadius = "50%";
-div.style.margin = "5%";
-div.style.float = "left";
-div.style.backgroundColor = getColor(); 
-document.body.appendChild(div);
-div.style.width = "100px";
-div.style.height = "100px";
-}
+  element = document.getElementById('c1');
+// let sub = document.getElementsById('c1');
+element.addEventListener('click', function(){
+console.log("e")
+alert( 'Всем привет!' );
 
 
-
-function example(){
-let sub = document.getElementsByClassName('main');
-let but = document.getElementById("lapup");
-let butcl = document.getElementById("lapclose");
-let h=0;
-but.addEventListener('click', function(){
-    
-    for(i=0;i<=sub.length; i++)
-     {
-     sub[i].style.display = 'block';}
 })
-butcl.addEventListener('click', function(){
-    for(i=0;i<=sub.length; i++)
-     {
-     sub[i].style.display = 'none';
-     }
-     }
+// sub.addEventListener('click', function(){
+    
+//     for(i=0;i<=sub.length; i++)
+//      {console.log("e")
+//     alert( 'Всем привет!' );
+//      }
+// })
+// butcl.addEventListener('click', function(){
+//     for(i=0;i<=sub.length; i++)
+//      {
+//      sub[i].style.display = 'none';
+//      }
+//      }
     
 
-)
-}
+
+
 ///////////////////////////////////////////////////////////////
